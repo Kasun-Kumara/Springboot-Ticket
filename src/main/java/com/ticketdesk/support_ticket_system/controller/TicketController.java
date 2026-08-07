3da490deb3770beb08b48cc204f29cd6ca5122cf
Kasun-Kumara <kasunkumarakrk@gmail.com>
2026-08-07 14:07:02 +0530

    multiple json responses using java Lists

--- src/main/java/com/ticketdesk/support_ticket_system/controller/TicketController.java
+++ src/main/java/com/ticketdesk/support_ticket_system/controller/TicketController.java
@@ -1,5 +1,6 @@
 package com.ticketdesk.support.controller;
 
+import java.util.List;
 import com.ticketdesk.support_ticket_system.model.Ticket;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketController {
 
     @GetMapping
-    public Ticket getTickets() {
-        Ticket ticket =new Ticket(1L,"WiFi not working","Unable to connect to uni WiFi","OPEN");
-        return ticket;
+    public List<Ticket> getTickets() {
+        
+        Ticket ticket1 = new Ticket(
+                1L,
+                "WiFi not working",
+                "Unable to connect to university WiFi",
+                "OPEN"
+        );
+
+        Ticket ticket2 = new Ticket(
+                2L,
+                "Cannot login",
+                "Password is not being accepted",
+                "IN_PROGRESS"
+        );
+        return List.of(ticket1,ticket2);
     }
 }
