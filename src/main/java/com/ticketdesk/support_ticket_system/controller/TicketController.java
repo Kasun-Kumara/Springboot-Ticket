208cc2ff1490f47dab2cb07c89994d7797e176fd
Kasun-Kumara <kasunkumarakrk@gmail.com>
2026-08-07 15:06:04 +0530

    added the arraylist to temporary store the tickets

--- src/main/java/com/ticketdesk/support_ticket_system/controller/TicketController.java
+++ src/main/java/com/ticketdesk/support_ticket_system/controller/TicketController.java
@@ -1,6 +1,7 @@
 package com.ticketdesk.support.controller;
 
 import java.util.List;
+import java.util.ArrayList;     
 import com.ticketdesk.support_ticket_system.model.Ticket;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,37 +11,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/api/tickets")
 public class TicketController {
-
+    private List<Ticket> tickets = new ArrayList<>();
     @GetMapping()
     public List<Ticket> getTickets(){
-        
-        Ticket ticket1 = new Ticket(
-                1L,
-                "WiFi not working",
-                "Unable to connect to university WiFi",
-                "OPEN"
-        );
-
-        Ticket ticket2 = new Ticket(
-                2L,
-                "Cannot login",
-                "Password is not being accepted",
-                "IN_PROGRESS"
-        );
-        return List.of(ticket1,ticket2);
+        return tickets;
     }
     @GetMapping("/{id}")
     public Ticket getTicketById(@PathVariable Long id) {
-        
-        Ticket ticket=new Ticket(id,
-            "WiFi not working",
-            "Unable to connect to university WiFi",
-            "OPEN");
-        return ticket;
+        for (Ticket ticket : tickets){
+            if(ticket.getId().equals(id)){
+                return ticket;
+            }
+        }
+        return null;
     }
 
     @PostMapping
     public Ticket createTicket(@RequestBody Ticket ticket) {
+        tickets.add(ticket);
         return ticket;
     }
 }
